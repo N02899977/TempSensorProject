9d3bde3428ae28db9c0c5d92cab97fc34506e40f
Jabari <jabaridash>
2016-05-04 19:47:43 -0400

    The getAverage() function returns the default value, not the computed value. This probably has something to do with the javascript interpreter.

--- src/mvc/controller/getTempJSON.js
+++ src/mvc/controller/getTempJSON.js
@@ -1,7 +1,12 @@
+function isNumber(obj) { return !isNaN(parseFloat(obj)) }
+
 // Returns the average value when passed an integer array
 function getAverage(room) {		
 	// initialize sum to 0
 	var sum = 0;
+	
+	// If you set the default value to 20, it will graph all 20s
+	var average = 20;
 	length = 0;
 	
 	climate_params = 'http://cs.newpaltz.edu/~loweb/pi/api/climate.php?climate=true&building=BLI&room=' + room;
@@ -16,13 +21,18 @@ function getAverage(room) {
 	        
 	    sum += parseFloat(packet.tempF);		   
 	    });
+      
+      // Despite the fact that I recalculate average here, it still returns 20 
+      // I suppose this is because it is hard coded
+      average = (sum/length).toFixed(3);
+      
       console.log("Room:" + room);
       console.log("Sum: " + sum);
       console.log("Length: " + length);
-      console.log("Average: " + sum/length);
+      console.log("Average: " + average);
       console.log("");
 	});
 
 	// Return the sum divided by the length (average)
-	return sum/length;
+	return average;
 }
