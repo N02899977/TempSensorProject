2c3c9f1698d56d46afac4c87ca3c2e564a24896e
Jabari <jabaridash>
2016-05-07 02:31:44 -0400

    Changed HTML references such as index.html to ../view/html because html files were nested into the view folders.

--- src/mvc/inc/nav.php
+++ src/mvc/inc/nav.php
@@ -8,15 +8,16 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="index.html">Temp Sensor Project</a>
+      <a class="navbar-brand" href="../view/index.html">Temp Sensor Project</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="home"><a href="index.html">Home</a></li>
-        <li class="temptime"><a href="tempVersusTime.html">Temp vs Time</a></li>
-        <li class="floorgraph"><a href="averageFloorTemp.html">Floor Temp</a></li>
+        <li class="home"><a href="../view/index.html">Home</a></li>
+        <li class="temptime"><a href="../view/tempVersusTime.html">Temp vs Time</a></li>
+        <li class="floorgraph"><a href="../view/averageFloorTemp.html">Average Floor Temp</a></li>
+        <li class="download"><a href="../view/downloadData.html">Download Data</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
